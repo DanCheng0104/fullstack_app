c189a9d766b998f09ca4a38fd1abb2e4fcf165b1
dcheng0104@gmail.com <dcheng0104@gmail.com>
2018-03-22 14:11:47 -0700

    add react and push to heroku 2nd

--- query.js
+++ query.js
@@ -1,25 +1,21 @@
 const promise = require('bluebird');
-const production = true;
+//const config = require('./config');
 const options = {
-    // Initialization Options
-    promiseLib: promise
-  };
+  // Initialization Options
+  promiseLib: promise
+};
+//console.log(config.connectionString);
+
+
 const pgp = require('pg-promise')(options);
 pgp.pg.defaults.poolSize = 20;
-
-if (production){
-    const connectionString = `${process.env.DATABASE_URL}?ssl=true`;
-    const db = pgp(connectionString);
-}
-else{
-    const config = require('./config');
-    var db = pgp(config.connectionString);
-}
-
+//var db = pgp(config.connectionString);
+const connectionString = `${process.env.DATABASE_URL}?ssl=true`;
+const db = pgp(connectionString);
 
 // add query functions
 function getAllPuppies(req, res, next) {
-    db.any('select name from nb_4326 limit 10;')
+    db.any('select ST_AsGeoJSON(geom) from nb_4326 limit 10;')
       .then(function (data) {
         res.status(200)
           .json({
