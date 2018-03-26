c202fea6d513458bf64e2293f261c3d45345fb1d
DAN CHENG <dcheng0104@gmail.com>
2018-03-25 22:23:45 -0700

    try to add color

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl'
 import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
 import '../css/style.css';
+import {color} from '../palette.json';
 
 mapboxgl.accessToken =  'pk.eyJ1IjoiZGNoZW5nMDEwNCIsImEiOiJjaXE0MDh2MHQwMG9xZnhtNGg0azVybGxtIn0.7jdNnbpd8kQI3qO1HfSnUg';
-
+console.log(color);
 class Map extends React.Component {
     // mapContainer = React.createRef();
-    state = { geos: {}}
+    state = {
+        geos: {},
+        year : 2014
+    };
     componentDidMount() {
       this.map = new mapboxgl.Map({
         container: this.mapContainer,
@@ -16,7 +20,8 @@ class Map extends React.Component {
         center: [ -118.382877, 34.014700]
 
       });
-      this.getGeos();
+
+      this.getAllUsage();
       this.map.on('load',()=>{
           console.log(this.state.geos);
           this.map.addSource("base_nb",{
@@ -29,17 +34,22 @@ class Map extends React.Component {
             "type": "fill",
             "source": "base_nb",
             "paint": {
-                "fill-color": "red",
-                "fill-opacity": 0.4
-            }
+                "fill-outline-color": "#e1cdb5",
+                'fill-opacity': 1,
+                "fill-color": "#888888"
+            },
+            "filter":  ["all",['==','year',this.state.year],['!=','usage',-9999]] 
           });
+
+         this.setFill(this.state.year);
+
       });
 
 
 
     }
 
-    getGeos=()=>{
+    getAllUsage=()=>{
         fetch('api/nbs')
         .then(res => res.json())
         .then(data => {
@@ -49,7 +59,7 @@ class Map extends React.Component {
             data.data.forEach(geo=>{
                 let feature = {};
                 feature.type = "Feature";
-                feature.properties = {"name":geo.name};
+                feature.properties = {"name":geo.name,"id":geo.id,"usage":geo.usage,"sqft":geo.sqft,"usage_med":geo.usage_med,"usage_med_sqft":geo.usage_med_sqft,"year":geo.year,"data_load_period_id":geo.data_load_period_id};
                 feature.geometry = JSON.parse(geo.st_asgeojson);
                 results.features.push(feature);
             })
@@ -57,6 +67,19 @@ class Map extends React.Component {
         });
     }
 
+    setFill =(year) =>{
+        this.map.setPaintProperty("nb-boundary",'fill-color',{
+            "property": "usage",
+            "stops": [
+              [353499,"#edf8fb"],
+              [974104,"#b2e2e2"],
+              [1895798,"#66c2a4"],
+              [3337788,"#2ca25f"],
+              [6053778,"#006d2c"]
+            ]
+        });
+
+    }
     componentWillUnmount() {
       this.map.remove();
     }
