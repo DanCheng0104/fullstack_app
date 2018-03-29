61676603704f65690403242fccd63cb3c5cd43ee
DAN CHENG <dcheng0104@gmail.com>
2018-03-29 15:39:09 -0700

    try to push to prod 3rd

--- query.js
+++ query.js
@@ -1,26 +1,26 @@
 const promise = require('bluebird');
 
 //prod
-const options = {
-    // Initialization Options
-    promiseLib: promise
-};
-const pgp = require('pg-promise')(options);
-pgp.pg.defaults.poolSize = 20;
-const connectionString = `${process.env.DATABASE_URL}?ssl=true`;
-const db = pgp(connectionString);
-//local
 // const options = {
 //     // Initialization Options
 //     promiseLib: promise
-//   };
+// };
 // const pgp = require('pg-promise')(options);
-// const config = require('./config');
-// const db = pgp(config.connectionString);
+// pgp.pg.defaults.poolSize = 20;
+// const connectionString = `${process.env.DATABASE_URL}?ssl=true`;
+// const db = pgp(connectionString);
+//local
+const options = {
+    // Initialization Options
+    promiseLib: promise
+  };
+const pgp = require('pg-promise')(options);
+const config = require('./config');
+const db = pgp(config.connectionString);
 
 // add query functions
 function getAllNbs(req, res, next) {
-    db.any('select *,st_asgeojson(geom) from water_usage_bld_nb_frontend where usetype in (\'commercial\',\'institutional\',\'other\',\'industrial\',\'res\',\'mixed_use\',\'all\') and data_load_period_id=2;')
+    db.any('select *,st_asgeojson(geom) from public.water_usage_bld_nb_frontend where usetype in (\'commercial\',\'institutional\',\'other\',\'industrial\',\'res\',\'mixed_use\',\'all\') and data_load_period_id=2;')
       .then(function (data) {
         res.status(200)
           .json({
@@ -35,7 +35,7 @@ function getAllNbs(req, res, next) {
 }
 
 function getAllData(req, res, next) {
-  db.any('select id,usetype,usage_int,sqft,usage_med,usage_med_sqft,year,data_load_period_id,name from water_usage_bld_nb_frontend where data_load_period_id=2;')
+  db.any('select id,usetype,usage_int,sqft,usage_med,usage_med_sqft,year,data_load_period_id,name from public.water_usage_bld_nb_frontend where data_load_period_id=2;')
     .then(function (data) {
       res.status(200)
         .json({
