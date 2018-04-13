77c0b3a393754257482fac78fe1911d6591eabe5
dcheng0104@gmail.com <dcheng0104@gmail.com>
2018-04-12 17:02:16 -0700

    one more commit

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -19,13 +19,8 @@ class BarChart extends Component {
     const node = this.node;
     var s = d3.selectAll('svg');
     s.remove();
-    const data = [
-        {month: "Q1-2016", apples: 3840, bananas: 1920, cherries: 1960, dates: 400},
-        {month: "Q2-2016", apples: 1600, bananas: 1440, cherries: 960, dates: 400},
-        {month: "Q3-2016", apples:  640, bananas:  960, cherries: 640, dates: 600},
-        {month: "Q4-2016", apples:  320, bananas:  480, cherries: 640, dates: 400},
-        {month: "Q5-2016", apples:  320, bananas:  480, cherries: 640, dates: 400}
-      ];
+    
+    // const data = this.props.chartData;
       
     const series = d3.stack()
           .keys(["apples", "bananas", "cherries", "dates"])
